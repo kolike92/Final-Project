6b167b00a5fa95b40d14da2fcb57d4e02f72e2c1
Rebecca Graber <rebeccagraber@dhcp-wifi-8021x-168-122-15-84.bu.edu>
2016-12-01 16:31:41 -0500

    Better BUEvent and BuddyUser

--- app/src/main/java/com/BUddy/android/BUEvent.java
+++ app/src/main/java/com/BUddy/android/BUEvent.java
@@ -1,32 +1,70 @@
 package com.BUddy.android;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by rebeccagraber on 11/1/16.
  */
 
-public class BUEvent {
+public class BUEvent implements Parcelable{
     private Date eventDate;
     private String eventTitle;
     private String eventDetails;
-    private  String participantNum;
+    private  int maxParticipants;
     private int category;
     private String location;
+    private List<String> participants;
+    private String creator;
+    private String firebaseId; //it's easier to store this inside the object
+
+    private BUEvent(Parcel in) {
+        participants = new ArrayList<>();
+        eventTitle = in.readString();
+        eventDetails = in.readString();
+        maxParticipants = in.readInt();
+        category = in.readInt();
+        location = in.readString();
+        in.readStringList(participants);
+        creator = in.readString();
+        firebaseId = in.readString();
+
+
+    }
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public BUEvent createFromParcel(Parcel in) {
+            return new BUEvent(in);
+        }
+
+        @Override
+        public BUEvent[] newArray(int size) {
+            return new BUEvent[0];
+        }
+
+    };
+
 
     public BUEvent()
     {
-
+        this.participants = new ArrayList<String>();
     }
 
-    public BUEvent(Date d, String title, String number, String details, int category, String location)
+    public BUEvent(Date d, String title, int number, String details, int category, String location, String creator)
     {
         eventDate = d;
         eventTitle = title;
-        participantNum = number;
+        maxParticipants = number;
         eventDetails = details;
         this.category = category;
         this.location = location;
+        participants = new ArrayList<String>();
+        this.creator = creator;
     }
 
 
@@ -63,12 +101,12 @@ public class BUEvent {
         this.eventTitle = eventTitle;
     }
 
-    public String getParticipantNum() {
-        return participantNum;
+    public int getMaxParticipants() {
+        return maxParticipants;
     }
 
-    public void setParticipantNum(String participantNum) {
-        this.participantNum = participantNum;
+    public void setMaxParticipants(int maxParticipants) {
+        this.maxParticipants = maxParticipants;
     }
 
     public String getLocation() {
@@ -78,4 +116,63 @@ public class BUEvent {
     public void setLocation(String location) {
         this.location = location;
     }
+
+    public List<String> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<String> participants) {
+        this.participants = participants;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getFirebaseId() {
+        return firebaseId;
+    }
+
+    public void setFirebaseId(String firebaseId) {
+        this.firebaseId = firebaseId;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeString(eventTitle);
+        dest.writeString(eventDetails);
+        dest.writeInt(maxParticipants);
+        dest.writeInt(category);
+        dest.writeString(location);
+        dest.writeStringList(participants);
+        dest.writeString(creator);
+        dest.writeString(firebaseId);
+        /*
+         private Date eventDate;
+    private String eventTitle;
+    private String eventDetails;
+    private  int maxParticipants;
+    private int category;
+    private String location;
+    private List<String> participants;
+    private String creator;
+    private String firebaseId; //it's easier to store this inside the object
+
+         */
+        if(eventDate != null) {
+            dest.writeString(StaticConstants.SDF.format(eventDate));
+        }
+    }
+
+
 }
