c7ed9a9c2d2a7ec7586567c2f46d61384453f835
Rebecca Graber <rebeccagraber@dhcp-wifi-8021x-155-41-127-163.bu.edu>
2016-12-04 11:28:58 -0500

    removing fake login button

--- app/src/main/java/com/BUddy/android/MainActivity.java
+++ app/src/main/java/com/BUddy/android/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements  GoogleApiClient.
     private LoginButton loginButton;
     private CallbackManager callbackManager;
     private GoogleApiClient mGoogleApiClient;
-    private Button btnBULogin, btnFakeSignIn;
+    private Button btnBULogin;
     private static final int RC_SIGN_IN = 9001;
     private static final int FB_SIGN_IN = 9002;
     private TextView tvError;
@@ -90,69 +90,6 @@ public class MainActivity extends AppCompatActivity implements  GoogleApiClient.
         loginButton = (LoginButton) findViewById((R.id.login_button));
         loginButton.setReadPermissions("email");
         btnBULogin = (Button) findViewById(R.id.btnLogin);
-        btnFakeSignIn = (Button) findViewById(R.id.btnFakeSignIn);
-        btnFakeSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                email = "fakeemail@bu.edu";
-                user = new BuddyUser();
-                user.setEmail(email);
-                Query q = dbRef.orderByChild("email").equalTo(email);
-                q.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        if(!dataSnapshot.exists())
-                        {
-                            user = new BuddyUser();
-                            user.setEmail(email);
-                            DatabaseReference eventRef = dbRef.push();
-                            user.setFirebaseId(eventRef.getKey());
-                            //add new user to db
-                            eventRef.setValue(user);
-                            Intent home = new Intent(getApplicationContext(), HomeActivity.class);
-                            home.putExtra(StaticConstants.USER_KEY,user);
-                            home.putExtra("something","else");
-
-
-                            startActivity(home);
-                        }
-                        else
-                        {
-                            Iterable<DataSnapshot> childSnap = dataSnapshot.getChildren();
-                            for(DataSnapshot d: childSnap) { //there should only be one...
-                                user = d.getValue(BuddyUser.class);
-                                if(user.getFirebaseId() == null || user.getFirebaseId().equals(""))
-                                {
-                                    user.setFirebaseId(d.getKey());
-
-                                    DatabaseReference userRef = dbRef.child(d.getKey());
-                                    userRef.setValue(user, new DatabaseReference.CompletionListener() {
-                                        @Override
-                                        public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
-                                            Log.d(StaticConstants.TAG, "sup");
-                                        }
-                                    });
-                                    Intent home = new Intent(getApplicationContext(), HomeActivity.class);
-                                    home.putExtra(StaticConstants.USER_KEY,user);
-                                    home.putExtra("something","else");
-
-
-                                    startActivity(home);
-                                }
-                            }
-
-                        }
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        tvError.setText(getString(R.string.unknownSignInError));
-                    }
-                });
-
-            }
-        });
 
         tvError = (TextView) findViewById(R.id.tvError);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
