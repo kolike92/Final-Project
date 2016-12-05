a5a3fbf170a94c37af1c9032a121476d51c6c957
Qi Wang <qiwangsophia@gmail.com>
2016-12-05 18:00:48 -0500

    inner

--- app/src/main/java/com/BUddy/android/InnerActivity.java
+++ app/src/main/java/com/BUddy/android/InnerActivity.java
@@ -1,8 +1,8 @@
 package com.BUddy.android;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -26,7 +26,7 @@ public abstract class InnerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         callbackManager = CallbackManager.Factory.create();
-
+        FacebookSdk.sdkInitialize(getApplicationContext());
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
