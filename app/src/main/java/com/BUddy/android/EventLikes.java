5affa5bab9956afb10d52afc508af9929b597d38
Qi Wang <qiwangsophia@gmail.com>
2016-12-05 17:59:55 -0500

    eventlike

--- app/src/main/java/com/BUddy/android/EventLikes.java
+++ app/src/main/java/com/BUddy/android/EventLikes.java
@@ -1,8 +1,68 @@
 package com.BUddy.android;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 /**
  * Created by Sophia_ on 12/5/16.
  */
 
-public class EventLikes {
+public class EventLikes extends AppCompatActivity {
+    private TextView tvEventLikes;
+    private ListView lvEventLikes;
+    private ListAdapter lvAdapter;
+
+    private ArrayList<BUEvent> events;
+
+
+    /* events database connection setup*/
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference dbEvent = db.getReference("events/");
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventlikes);
+
+        tvEventLikes = (TextView) findViewById(R.id.tvEventLikes);
+        lvEventLikes = (ListView) findViewById(R.id.lvEventLikes);
+        events = new ArrayList<BUEvent> ();
+
+
+        dbEvent.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child:dataSnapshot.getChildren()) {
+                    try {
+                        String likes = child.child("likes").getValue().toString();
+                        likes = likes.substring(1,(likes.length()-1));
+                        String[] likes_list = likes.split(",");
+
+                    } catch (NullPointerException e) {}
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
 }
